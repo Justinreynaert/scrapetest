1b8fa5db94df088892097552a36cbde13aca8276
Justin <justin.reynaert@gmail.com>
2017-05-03 22:45:23 +0200

    db during scrape

--- server.js
+++ server.js
@@ -2,13 +2,42 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const osmosis = require('osmosis');
+const mongoose = require('mongoose');
 
-const lfgdata = [];
+const lfgdata = {};
+
+mongoose.connect('mongodb://localhost/wowprogtracker');
+
+//Character Schema
+const Character = new mongoose.Schema({
+    name: String,
+    dateupdated: String,
+    info: {
+        style:  String,
+        realm: String,
+        guild: String,
+        commetary: String,
+        class:String,
+        ilvl: Number,
+        dateupdated: String
+    }
+},{
+    versionKey: false
+});
+
+const characterModel = mongoose.model('Character', Character); // pluralization
 
 app.get('/scrape', (req,res) => {
 
+    let totalExisting = 0;
+    let timestamp = JSON.stringify(Date.now());
+
+    res.send(timestamp);
+    for (let i = 0; i < 42; i++) {
+
+        // SCRAPE TIME
     osmosis
-        .get('https://www.wowprogress.com/gearscore/?lfg=1')
+        .get('http://www.wowprogress.com/gearscore/eu/char_rating/next/' + i + '/lfg.1/raids_week./lang.en')
         .find('a.character')
         .follow('@href')
         .set({
@@ -20,36 +49,77 @@ app.get('/scrape', (req,res) => {
                 'style':  '.raiding_style',
                 'realm': 'a.nav_link',
                 'guild': 'a.guild',
-                'commetary': 'div.charCommetary',
                 'class':'i span',
+                'ilvl':'div.gearscore',
+                'dateupdate':'span.datetime'
             }
-            //'subcategory':  'div.breadbox > span[4]',
-            //'date':         'time@datetime',
-            //'latitude':     '#map@data-latitude',
-            //'longitude':    '#map@data-longitude',
-            //'images':       ['img@src']
+
         })
         .data(function(data) {
-            console.log(data);
-            lfgdata.push(data);
 
-            //write json to file
-            fs.writeFile("./tmp/test.txt",JSON.stringify(lfgdata), function(err) {
-                if (err)
-                {
-                    return console.log(err)
+            //track exisiting users in DB
+
+
+            //console.log(data);
+
+
+            /// DATA FORMATTING --
+            //remove "ITEM LEVEL" --
+            if ( data.info.ilvl) {
+            data.info.ilvl = data.info.ilvl.replace('Item Level: ','');
+            }
+
+            //PUSH TO MONGODB
+            // Check if character exists
+            characterModel.count({name: data.name}, function (err,count) {
+                //character exists - do nuthing
+                if(count>0){
+                    console.log('exisiting user');
+                    totalExisting++;
+
                 }
-            });
+                else // new char -- add to db
+                    {
+                    const character = new characterModel({
+                        name: data.name,
+                        info: {
+                            style:  data.info.style,
+                            realm: data.info.realm,
+                            guild: data.info.guild,
+                            class: data.info.class,
+                            ilvl: data.info.ilvl,
+                            dateupdate: data.info.dateupdate
+                        },
+                        dateupdated: timestamp
+                    })
+
+                    character.save(function(err) {
+                        if (!err) {
+                            return console.log('Character added');
+                        } else {
+                            return console.log(err);
+                        }
+                    })
+
+                }
 
 
 
 
+            })
         })
+
         .log(console.log)
         .error(console.log)
         .debug(console.log);
 
-        res.send('done');
+
+
+    }
+
+    console.log('finished');
+
+    res.send('done');
 
 
 });
