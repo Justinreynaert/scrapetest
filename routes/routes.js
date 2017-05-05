a5d943896a5c5477d5c9eb91203e1b75adf7b46d
justin <justin.reynaert@gmail.com>
2017-05-05 20:29:55 +0200

    Update

--- routes/routes.js
+++ routes/routes.js
@@ -277,6 +277,8 @@ module.exports = (app) => {
         )
     });
 
+
+
 };
 
 
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 config = require('./config/database');
 
@@ -11,10 +12,28 @@ mongoose.connect(config.database);
 // Cors Middelware
 app.use(cors());
 
-app.use(bodyparser.json());
+
+
+//body parser middleware
+
+//BodyParser Middelware configuration
+app.use(bodyParser.json());
+
+//parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
 
 
 require('./routes/routes')(app);
+
+app.all('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 
 
